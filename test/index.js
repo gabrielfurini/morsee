4d13503e0526b6b66cf4abf37eb49915ec230899
Gabriel Furini <gabrielfurini@users.noreply.github.com>
2020-12-18 12:39:01 -0300

    Chore/Updating all dev packages and adding Prettier (#14)
    * Chore: updating packages
    
    * Chore: "equal" was deprecated. Using "strictEqual" instead
    
    * Feat: adding Prettier

--- test/index.js
+++ test/index.js
@@ -1,49 +1,49 @@
 const assert = require('assert')
 const { toMorse } = require('../src/dictionary')
-const { encode, decode } = require('../')
+const { encode, decode } = require('..')
 
 describe('Morsee', () => {
   describe('to encode', () => {
     it('should return the correspondent latin char in morse dictionary', () => {
-      assert.equal(encode('h'), toMorse.h)
+      assert.strictEqual(encode('h'), toMorse.h)
     })
 
     it('should return a word, with chars splitted by blank spaces', () => {
-      assert.equal(encode('hi'), `${toMorse.h} ${toMorse.i}`)
+      assert.strictEqual(encode('hi'), `${toMorse.h} ${toMorse.i}`)
     })
 
     it('should return a sentence, with words splitted by slashes', () => {
-      assert.equal(encode('hi friend'), `${toMorse.h} ${toMorse.i} ${toMorse[' ']} ${toMorse.f} ${toMorse.r} ${toMorse.i} ${toMorse.e} ${toMorse.n} ${toMorse.d}`)
+      assert.strictEqual(encode('hi friend'), `${toMorse.h} ${toMorse.i} ${toMorse[' ']} ${toMorse.f} ${toMorse.r} ${toMorse.i} ${toMorse.e} ${toMorse.n} ${toMorse.d}`)
     })
 
     it('should return punctuation chars', () => {
-      assert.equal(encode('hi!'), `${toMorse.h} ${toMorse.i} ${toMorse['!']}`)
+      assert.strictEqual(encode('hi!'), `${toMorse.h} ${toMorse.i} ${toMorse['!']}`)
     })
 
     it('should return a new line char', () => {
-      assert.equal(encode('h\ni'), `${toMorse.h} ${toMorse['\n']} ${toMorse.i}`)
+      assert.strictEqual(encode('h\ni'), `${toMorse.h} ${toMorse['\n']} ${toMorse.i}`)
     })
   })
 
   describe('to decode', () => {
     it('should return the correspondent morse char in latin dictionary', () => {
-      assert.equal(decode('....'), 'h')
+      assert.strictEqual(decode('....'), 'h')
     })
 
     it('should return a word', () => {
-      assert.equal(decode('.... ..'), 'hi')
+      assert.strictEqual(decode('.... ..'), 'hi')
     })
 
     it('should return a sentence, with words splitted by blank spaces', () => {
-      assert.equal(decode('.... .. / ..-. .-. .. . -. -..'), 'hi friend')
+      assert.strictEqual(decode('.... .. / ..-. .-. .. . -. -..'), 'hi friend')
     })
 
     it('should return punctuation chars', () => {
-      assert.equal(decode('.... .. -.-.--'), 'hi!')
+      assert.strictEqual(decode('.... .. -.-.--'), 'hi!')
     })
 
     it('should return a new line char', () => {
-      assert.equal(decode('.... .-.- ..'), 'h\ni')
+      assert.strictEqual(decode('.... .-.- ..'), 'h\ni')
     })
   })
 })
