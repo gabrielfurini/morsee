e704a5ab8322b4f7fc4bd7d42176ab5b52f58f60
Bas950 <me@bas950.com>
2022-02-08 21:14:59 +0100

    types: add type declaration file

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Encodes a given string into morse code.
+ *
+ * @param str The input string to encode.
+ * @returns A string of the encoded input.
+ */
+export function encode(str: string): string;
+
+/**
+ * Decodes a given morse code string into plain text.
+ *
+ * @param str The input string to decode.
+ * @returns A string of the decoded input.
+ */
+export function decode(str: string): string;
